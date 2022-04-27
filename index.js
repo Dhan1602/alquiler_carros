6003d0cca2cb35545f0a637c62fd81460fc344a3
Daniel <danielesteban2004@gmail.com>
2022-04-26 22:03:59 -0500

    ya funcionan ambos botones

--- index.js
+++ index.js
@@ -11,26 +11,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
-app.post("/alquiler",function(req, res){
+app.post("/alquiler", function (req, res) {
     a++;
     var fecha = new Date();
-    var hoy = fecha.getDate()+"/"+(fecha.getMonth()+1)+"/"+fecha.getFullYear();
-    res.send("<tr id='"+a+"'>"+
-            "<td>"+hoy+"</td>"+
-            "<td>"+req.body.marca+"</td>"+
-            "<td>"+req.body.modelo+"</td>"+
-            "<td>"+req.body.color+"</td>"+
-            "<td>$"+req.body.valor+"</td>"+
-            "<td>"+req.body.cantidadh+"</td>"+
-            "<td>$"+(req.body.cantidadh*req.body.valor)+"</td>"+
-            "<td><img width='300px' src='"+req.body.imagen+"'></td>"+
-            "<td><button type='submit' class='eliminar' value='"+a+"'>Eliminar</button><br><br><button type='submit' value='"+a+"' class='actualizar'>Actualizar</button></td></tr>"
-            );
+    var hoy = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
+    res.send("<tr id='" + a + "'>" +
+        "<td>" + hoy + "</td>" +
+        "<td><input style='width: 300px; border: none;' value='" + req.body.marca + "'disabled></input></td>" +
+        "<td><input style='width: 300px; border: none;' value='" + req.body.modelo + "'disabled></input></td>" +
+        "<td><input style='width: 300px; border: none;' value='" + req.body.color + "'disabled></input></td>" +
+        "<td>$<input type='number' style='width: 300px; border: none;' value='" + req.body.valor + "'disabled></input></td>" +
+        "<td><input type='number' style='width: 300px; border: none;' value='" + req.body.cantidadh + "'disabled></input></td>" +
+        "<td>$" + (req.body.cantidadh * req.body.valor) + "</td>" +
+        "<td><img width='300px' src='" + req.body.imagen + "'></td>" +
+        "<td><button type='submit' class='eliminar' value='" + a + "'>Eliminar</button><br><br><button type='submit' value='" + a + "' class='actualizar'>Actualizar</button></td></tr>"
+    );
 })
 
 //Botones
 app.post("/eliminar", (req, res) => {
     res.send("Se ha eliminado correctamente");
 })
+
+app.post("/actualizar", (req, res) => {
+    console.log("Se está modificando un carro");
+    res.send("se esta modificando un carro")
+})
+
 
 app.listen(3000);
